74620859bae05568c31995322bb1b9d840f1ab67
Somesh <97947690+Somesh140@users.noreply.github.com>
2023-02-02 17:59:27 +0530

    Update first_dag.py

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,1 +1,18 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import pandas as pd
 
+
+def first_function_execute():
+  print("hello world")
+  return "hello world"
+
+with DAG(
+   dag_id="my_first_dag",
+    schedule_interval="0 10 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
+    catchup=False,
+) as dag:
+  
